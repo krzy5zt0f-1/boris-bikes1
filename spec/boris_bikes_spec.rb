7e215bd7d6ae940680ba3a60a4c63903ad815d36
GDCStoney <g.stoney@btinternet.com>
2020-10-13 15:20:11 +0100

    missed update

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,8 +1,5 @@
 require 'boris_bikes'
 
 RSpec.describe DockingStation do
-  #it "#Checking for the 'release_bike' method" do
-    #expect(subject).to respond_to(:release_bike)
     it {is_expected.to respond_to(:release_bike)}
-  #end
 end
